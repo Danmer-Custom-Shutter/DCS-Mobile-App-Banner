783e98079c89f2adb3cdb3db13b242fca33d3d0c
Bradford Knowlton <brad@ucanbonline.net>
2014-08-05 18:29:30 -0600

    Added large text css

--- js/mobile-app-banner.js
+++ js/mobile-app-banner.js
@@ -6,14 +6,14 @@ jQuery(document).ready(function($) {
 	var isIpad = ua.indexOf("ipad") > -1; //&& ua.indexOf("mobile");
 	if (isAndroid) {
 		// Do something!
-		$('#container').before('<div id="mobile-app-alert"><div class="dashicons dashicons-no-alt"></div><img src="/wp-content/plugins/DCS-Mobile-App-Banner/img/app-icon.png" class="app-logo" /><div>Shutter Shopper for iPhone<br/><a href="https://play.google.com/store/apps/details?id=com.chameleonpower.shuttershopper" target="_BLANK" class="view-link">View</a>Danmer Inc.<br/><div class="dashicons-row"><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-empty"></div></div><div class="small-text">FREE &mdash; On the App Store</div></div></div>');
+		$('#container').before('<div id="mobile-app-alert"><div class="dashicons dashicons-no-alt"></div><img src="/wp-content/plugins/DCS-Mobile-App-Banner/img/app-icon.png" class="app-logo" /><div>Shutter Shopper for Android<br/><a href="https://play.google.com/store/apps/details?id=com.chameleonpower.shuttershopper" target="_BLANK" class="view-link">View</a>Danmer Inc.<br/><div class="dashicons-row"><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-empty"></div></div><div class="small-text">FREE &mdash; On the App Store</div></div></div>');
 		$('#mobile-app-alert').slideDown('slow');
 		
 	}
 	if (isIphone || isIpad) {
 		// Do something!
 		
-		$('#container').before('<div id="mobile-app-alert"><div class="dashicons dashicons-no-alt"></div><img src="/wp-content/plugins/DCS-Mobile-App-Banner/img/app-icon.png" class="app-logo" /><div>Shutter Shopper for iPhone<br/><a href="https://itunes.apple.com/us/app/shutter-shopper/id890489820?mt=8" target="_BLANK" class="view-link">View</a>Danmer Inc.<br/><div class="dashicons-row"><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-empty"></div></div><div class="small-text">FREE &mdash; On the App Store</div></div></div>');
+		$('#container').before('<div id="mobile-app-alert"><div class="dashicons dashicons-no-alt"></div><img src="/wp-content/plugins/DCS-Mobile-App-Banner/img/app-icon.png" class="app-logo" /><div><div class="large-text">Shutter Shopper for iPhone</div><a href="https://itunes.apple.com/us/app/shutter-shopper/id890489820?mt=8" target="_BLANK" class="view-link">View</a>Danmer Inc.<br/><div class="dashicons-row"><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-filled"></div><div class="dashicons dashicons-star-empty"></div></div><div class="small-text">FREE &mdash; On the App Store</div></div></div>');
 		$('#mobile-app-alert').slideDown('slow');
 		
 	}
